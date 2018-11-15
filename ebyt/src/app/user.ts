b454ae6cff2de3c0e9ceaa302a608ba0fabee55c
Nicolas <nicolas.schumacher@wanadoo.fr>
2018-11-15 17:39:22 +0100

    harmonisation nom attribut back et front

--- ebyt/src/app/user.ts
+++ ebyt/src/app/user.ts
@@ -2,15 +2,15 @@ export class User {
 	id: number;
 	username: string;
 	password: string;
-	nom: string;
-	prenom: string;
-	photo: string;
+	lastname: string;
+	firstname: string;
+	picture: string;
 
-	constructor(username?: string, password?: string, nom?: string, prenom?: string, photo?: string){
+	constructor(username?: string, password?: string, lastname?: string, firstname?: string,	picture?: string){
 		this.username = username === undefined ? null : username;
 		this.password = password === undefined ? null : password;
-		this.nom = nom === undefined ? null : nom;
-		this.prenom = prenom === undefined ? null : prenom;
-		this.photo = photo === undefined ? null : photo;
+		this.lastname = lastname === undefined ? null : lastname;
+		this.firstname = firstname === undefined ? null : firstname;
+		this	picture =	picture === undefined ? null :	picture;
 	}
 }
