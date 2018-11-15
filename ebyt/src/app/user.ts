31cfbb4f617beb36ea850d4ebe4cacc38c6db71a
Nicolas <nicolas.schumacher@wanadoo.fr>
2018-11-15 17:00:18 +0100

    mettre nom attribut identique entre front et back

--- ebyt/src/app/user.ts
+++ ebyt/src/app/user.ts
@@ -1,14 +1,14 @@
 export class User {
 	id: number;
-	mail: string;
-	mdp: string;
+	username: string;
+	password: string;
 	nom: string;
 	prenom: string;
 	photo: string;
 
-	constructor(mail?: string, mdp?: string, nom?: string, prenom?: string, photo?: string){
-		this.mail = mail === undefined ? null : mail;
-		this.mdp = mdp === undefined ? null : mdp;
+	constructor(username?: string, password?: string, nom?: string, prenom?: string, photo?: string){
+		this.username = username === undefined ? null : username;
+		this.password = password === undefined ? null : password;
 		this.nom = nom === undefined ? null : nom;
 		this.prenom = prenom === undefined ? null : prenom;
 		this.photo = photo === undefined ? null : photo;
